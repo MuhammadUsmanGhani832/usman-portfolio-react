564f474e12586083332d9ec539887c4e9538e8fd
Muhammad Usman <mug832@gmail.com>
2024-03-28 16:00:04 +0500

    lets try again

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
-import { NavLink } from 'react-router-dom';
-import styles from './navbar.module.css'
+import { NavLink } from "react-router-dom";
+import styles from "./navbar.module.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+
 const Navbar = () => {
+  const openMenu = () => {
+  const navbar_links = document.getElementById("navbar_links");
+    navbar_links.style.right = 0;
+  };
+
+  const closeMenu = () => {
+  const navbar_links = document.getElementById("navbar_links");
+    navbar_links.style.right = "-300px";
+  };
+
+
   return (
-    <>
+    <div className={styles.container}>
       <div className={styles.navbar}>
         <h1>&#60;USMAN /&#62;</h1>
-        <div className={styles.navbar_links}>
+        <div className={styles.navbar_links} id="navbar_links">
+          <MdClose onClick={closeMenu}/>
           <NavLink
-          to='/'
+            to="/"
             className={({ isActive }) => {
               return isActive
                 ? `${styles.navbar_link} ${styles.active}`
@@ -17,7 +32,7 @@ const Navbar = () => {
             Home
           </NavLink>
           <NavLink
-          to={'/projects'}
+            to={"/projects"}
             className={({ isActive }) => {
               return isActive
                 ? `${styles.navbar_link} ${styles.active}`
@@ -25,14 +40,14 @@ const Navbar = () => {
             }}
           >
             Projects
-          </NavLink> 
+          </NavLink>
           <button>Download CV</button>
         </div>
-       
+        <GiHamburgerMenu onClick={() => openMenu()} />
       </div>
       <div className={styles.divider}></div>
-    </>
+    </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
